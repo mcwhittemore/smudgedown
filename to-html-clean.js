4f0ead7cb1a46b8e0c95c8f8832a6572ca02f908
Matthew Chase Whittemore <mcwhittemore@gmail.com>
2014-12-02 23:39:58 -0500

    Update to-html-clean.js

--- to-html-clean.js
+++ to-html-clean.js
@@ -10,7 +10,7 @@ process.stdin.on("end", function(data){
 	//var html = markdown.toHTML(md);
 	//console.log(html);
 	var baseBuffer = new Buffer(encodeURIComponent(md));
-	var comment = "<-- SMUDGEDOWN-START\n"+baseBuffer.toString('base64')+"\nSMUDGEDOWN-END -->";
+	var comment = "<!-- SMUDGEDOWN-START\n"+baseBuffer.toString('base64')+"\nSMUDGEDOWN-END -->";
 	
 	var html = markdown.toHTML(md);
 	var content = html+"\n"+comment;
